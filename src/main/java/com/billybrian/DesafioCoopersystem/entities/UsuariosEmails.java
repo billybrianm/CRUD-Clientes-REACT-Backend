76f5dbddcb287a62270b13ebb1f5408bc814ad32
billybrian <billybrianm@hotmail.com>
2021-09-16 19:20:11 -0300

    adicionadas classes dos emails

---
+++ src/main/java/com/billybrian/DesafioCoopersystem/entities/UsuariosEmails.java
@@ -0,0 +1,76 @@
+package com.billybrian.DesafioCoopersystem.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class UsuariosEmails {
+private @Id @GeneratedValue Long id;
+	
+	@ManyToOne
+	private Usuario usuario;
+	private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuariosEmails other = (UsuariosEmails) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "UsuariosEmails [id=" + id + ", usuario=" + usuario + ", email=" + email + "]";
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/billybrian/DesafioCoopersystem/repositories/UsuarioEmailsRepository.java
@@ -0,0 +1,9 @@
+package com.billybrian.DesafioCoopersystem.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.billybrian.DesafioCoopersystem.entities.UsuariosEmails;
+
+public interface UsuarioEmailsRepository extends CrudRepository<UsuariosEmails, Long>{
+
+}
