b195f1d97c3dd2bfae9b1465b1b52b384d0d779e
billybrian <billybrianm@hotmail.com>
2021-09-16 12:15:43 -0300

    token retornado ao fazer login

---
+++ src/main/java/com/billybrian/DesafioCoopersystem/dto/TokenDTO.java
@@ -0,0 +1,47 @@
+package com.billybrian.DesafioCoopersystem.dto;
+
+public class TokenDTO {
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TokenDTO other = (TokenDTO) obj;
+		if (token == null) {
+			if (other.token != null)
+				return false;
+		} else if (!token.equals(other.token))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TokenDTO [token=" + token + "]";
+	}
+	
+	
+	
+	
+}
