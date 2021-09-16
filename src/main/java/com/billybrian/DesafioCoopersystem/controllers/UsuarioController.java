e93c87e4288e7c71ea9b08d0014b04a08719e64a
billybrian <billybrianm@hotmail.com>
2021-09-16 11:42:12 -0300

    inicio dos testes unitarios do controller dos usuarios

---
+++ src/main/java/com/billybrian/DesafioCoopersystem/controllers/UsuarioController.java
@@ -0,0 +1,36 @@
+package com.billybrian.DesafioCoopersystem.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.billybrian.DesafioCoopersystem.entities.Usuario;
+import com.billybrian.DesafioCoopersystem.repositories.UsuarioRepository;
+
+@RestController
+public class UsuarioController {
+	
+	private final UsuarioRepository repository;
+	
+	UsuarioController(UsuarioRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping("/usuarios")
+	  List<Usuario> all() {
+		List<Usuario> ret = new ArrayList<>();
+		repository.findAll().forEach(ret::add);
+	    return ret;
+	}
+	
+	@PostMapping("/employees")
+	  Usuario newEmployee(@RequestBody Usuario novoUsuario) {
+	    return repository.save(novoUsuario);
+	}
+	
+	
+}
