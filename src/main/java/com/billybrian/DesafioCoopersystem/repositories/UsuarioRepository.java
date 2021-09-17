5a2f3511a2fbed01fd2684f8e9082f089c5ebda2
billybrian <billybrianm@hotmail.com>
2021-09-17 17:40:18 -0300

    sistema de login

--- src/main/java/com/billybrian/DesafioCoopersystem/repositories/UsuarioRepository.java
+++ src/main/java/com/billybrian/DesafioCoopersystem/repositories/UsuarioRepository.java
@@ -3,10 +3,14 @@ package com.billybrian.DesafioCoopersystem.repositories;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.billybrian.DesafioCoopersystem.dto.TokenDTO;
 import com.billybrian.DesafioCoopersystem.entities.Usuario;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {	
 	@Override
     @Query("select distinct usuario from Usuario usuario left join fetch usuario.telefones")
     Iterable<Usuario> findAll();
+	
+	@Query("select u.isAdmin from Usuario u where u.usuario =:usuario and u.senha =:senha")
+	boolean getAdmin(String usuario, String senha);
 }
